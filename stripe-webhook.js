3f30fce1cf1625f727772ae6a601f5618747e701
Chris <chrismiloyevic0703@gmail.com>
2026-04-27 13:18:54 +1000

    fix: subscription plan

--- stripe-webhook.js
+++ stripe-webhook.js
@@ -14,6 +14,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 const PLAN_MAP = {
   prod_U4tCikdA2Kr75c: 'Maker',
   prod_TWPbyx7qGRLd6J: 'The Growth Circle 🚀',
+  prod_QitoWsRWrfLg0e: 'Starter',
 
   prod_MfxwKxisQiI5pQ: 'Hobby',
   prod_RDrvOrlcGKv17S: 'Business',
